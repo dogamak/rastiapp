017c820f1dfd5225c56d0561a6f8c61273daef5f
Santtu Lempinen <santtu.lempinen@gmail.com>
2020-09-15 18:34:22 +0300

    Make cheating harder

--- src/components/MagicBag.js
+++ src/components/MagicBag.js
@@ -1,24 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import WordList from './WordList'
 import { pohina, normal } from '../wordlist'
+import { sampleSize } from 'lodash'
 import "../App.css"
 
 const MagicBag = () => {
-    const [clicked, setClicked] = useState(false)
+    const [pohinaList, setPohinaList]= useState([])
+    const [normalList, setNormalList]= useState([])
 
+    useEffect(() => {
+        setNormalList(JSON.parse(localStorage.getItem('normalwords')))
+        setPohinaList(JSON.parse(localStorage.getItem('pohinawords')))
+    }, [])
 
+    const chooseWords = () => {
+       const pohinawords = sampleSize(pohina, 3) 
+       const normalwords = sampleSize(normal, 2)
+       setPohinaList(pohinawords)
+       setNormalList(normalwords)
+       localStorage.setItem('pohinawords', JSON.stringify(pohinawords))
+       localStorage.setItem('normalwords', JSON.stringify(normalwords))
+    }
 
-    if (clicked) return (
+    if (pohinaList && normalList) return (
         <div className="wordList">
-            <WordList list={pohina} count={3} />
-            <WordList list={normal} count={2} />
+            <WordList list={pohinaList} />
+            <WordList list={normalList} />
         </div>
-        
     )
 
     return (
         <div className="buttonDiv">
-            <button onClick={() => setClicked(!clicked)} >ARVO!</button>
+            <button onClick={chooseWords} >ARVO!</button>
         </div>
     )
 }
--- src/components/WordList.js
+++ src/components/WordList.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import { sampleSize } from 'lodash'
 
-const WordList = ({ list, count }) => {
-    const sampledList = sampleSize(list, count)
+const WordList = ({ list }) => {
     const bgColors = ["#F4511E", "#FDD835", "#FFB300", "#FB8C00", "#43A047", "#7CB342", "#C0CA33", 
     "#039BE5", "#00ACC1", "#00897B", "#5E35B1", "#3949AB", "#1E88E5", "#E53935", "#D81B60", "#8E24AA"];
     bgColors.sort(() => 0.5 - Math.random());
@@ -19,7 +17,7 @@ const WordList = ({ list, count }) => {
     }
     return (
         <>
-            {sampledList.map((word, index) =>
+            {list.map((word, index) =>
                     <div style={{...itemStyle, backgroundColor: bgColors[index]}} key={word}>
                         {word.toUpperCase()}
                     </div>)
